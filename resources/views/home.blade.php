ca4b660af1e1a1be28a5bcaaa6464670114104e9
Arvil @DesktopPC <arvilmena@gmail.com>
2018-11-25 20:08:42 +0800

    init attempt

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,42 @@
 @extends('layout')
 
 @section('content')
+
+    <div class="container-fluid">
+        <nav id="headingNav" class="navbar navbar-expand-md navbar-dark bg-dark sticky-top">
+            <div class="container">
+                <span class="navbar-text">
+                    <a href="/" class="navbar-brand">f'lm House</a>
+                </span>
+                <button class="navbar-toggler" data-toggle="collapse" data-target="#mainNav">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div id="mainNav" class="collapse navbar-collapse ml-auto">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a href="/" class="nav-link" id="movies">Movies</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/login" class="nav-link" id="login">Log-in</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/register" class="nav-link" id="register">Register</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+
 	<div class="container-fluid">
 		<div class="container">
 			<div class="row">
-				<div class="col-sm-12 col-md-6">
+				<div class="col-sm-12 col col-12">
 					<img src="#" alt="poster">
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores distinctio, a incidunt veritatis molestias magni iure cum, maxime nulla, nihil adipisci expedita, veniam nostrum. Ratione repellat, temporibus voluptate atque. Libero.</p>
 				</div>
-				<div class="col-sm-12 col-md-6">
-					<div class="announcement-cont d-flex 
+				<div class="col-sm-12 col col-12">
+					<div class="announcement-cont d-flex
 						align-items-center justify-content-center">
 							<div class="">
 								<h1 class="">Announcement</h1>
@@ -21,58 +48,154 @@
 		</div>
 	</div>
 
-	<div id="movieCategory" class="container">
-		<div class="movie-category row text-center">
-			<a class="d-inline col-4 active-cat">Now Showing</a>
-			<a class="d-inline col-4">Comming Soon</a>
-			<a class="d-inline col-4">Search Movie</a>
-		</div>
-	</div>
 
-	<div class="container">
+	<div class="container" id="content">
 		<h1>Movie List</h1>
-		<div class="text-center row" id="movieList">
-{{-- 			<div class="col-sm-12 col-md-4 col-lg-3">
-
-				<img class="d-block" src="#" alt="movie">
-				<a href="/single-movie" class="link">Buy</a>				
-			</div> --}}
-			
-		</div>
+        <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Description</th>
+            <th scope="col">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            </tr>
+            <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            </tr>
+            <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            </tr>
+        </tbody>
+        </table>
 	</div>
+
+    <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="loginModalLabel">Login</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <form class="form-group" id="loginForm">
+        <div class="col col-12">
+            <label for="email">Email</label><br>
+            <input type="text" name="email" class="form-control" id="email">
+        </div>
+
+        <div class="col col-12">
+            <label for="password">Password</label><br>
+            <input type="password" name="password" class="form-control" id="password">
+        </div>
+     </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" id="sendLoginRequest">Login</button>
+      </div>
+    </div>
+  </div>
+</div>
 	<script>
-		
-		var myList = document.querySelector('#movieList');
 
-			const getMovie = () => {
-				return fetch('http://localhost:8001/movies')
-				.then(response => response.json())
-				.then(movies => {
-					JSON.stringify(movies);
-					for (var i = 0; i < movies.length; i++){
+        var app = (function( document, window, $, Mustache ) {
 
-						var listMovie = document.createElement('div');
-						listMovie.setAttribute('class', 'col-sm-12 col-md-4 col-lg-3');
-						var itemImg = document.createElement('img');
-						itemImg.classList.add('d-block');
-						itemImg.setAttribute('src','./svg/' + movies[i].id + '.jpg');
-						var itemAnchor = document.createElement('a');
-						itemAnchor.setAttribute('href', 'http://localhost:8000/single-movie/' + movies[i].id);
-						itemAnchor.innerHTML= 'Buy';
-						itemAnchor.classList.add('link');
+            function app( document, window, $, Mustache ) {
+                this.showLoginLink = $('#login');
+                this.loginModal = $('#loginModal');
+                this.loginForm = $('#loginForm');
+                this.sendLoginRequestLink = $('#sendLoginRequest');
+                this.invokeMovieList = $('#movies');
+                this.invokeRegister = $('#register');
+                this.authToken = false;
+                this.authName = null;
+            }
 
+            app.prototype.init = function() {
+                var that = this;
+                that.showLoginFormOnLinkClick();
+                that.sendLoginRequestOnLoginClick();
+            }
 
-						// listMovie.innerHTML = '<h3>' +  movies[i].name + '</h3>' + '<p>' + movies[i].description+ '</p></div>';
-						myList.appendChild(listMovie);
-						listMovie.appendChild(itemImg);
-						listMovie.appendChild(itemAnchor);
+            app.prototype.setAuthToken = function(token) {
+                this.authToken = token;
+            }
+            app.prototype.setAuthName = function(name) {
+                this.authName = name;
+            }
 
-					}
-				})
-			};
+            app.prototype.sendLoginRequestOnLoginClick = function() {
+                var that = this;
+                that.sendLoginRequestLink.on( 'click',  function(e) {
+                    console.log('sending Login Request');
+                    e.preventDefault();
+                    let emailValue = that.loginForm.find( 'input[name="email"]' ).val();
+                    let passwordValue = that.loginForm.find( 'input[name="password"]' ).val();
+                    let payload = { 'email' : emailValue, 'password' : passwordValue };
+                    console.log(payload);
+                    const loginEndPoint = 'http://127.0.0.1:8001/api/login';
+                    that.helperPostData(loginEndPoint, payload)
+                    .then( function(data) {
+                        that.setAuthToken( data.api_token );
+                        that.loginModal.modal('hide');
+                        console.log( 'authToken is set to ' + that.authToken );
+                    } ) // JSON-string from `response.json()` call
+                    .catch(error => console.error(error));
+                });
+            }
 
-			getMovie();
-		
+            app.prototype.showLoginFormOnLinkClick = function() {
+                var that = this;
+                that.showLoginLink.on( 'click',  function(e) {
+                    e.preventDefault();
+                    that.loginModal.modal();
+                } );
+            }
+
+            app.prototype.listMovies = function() {
+                var that = this;
+
+            }
+
+            /**
+             * @see https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#Supplying_request_options
+             */
+            app.prototype.helperPostData = function(url = ``, data = {}) {
+            // Default options are marked with *
+                return fetch(url, {
+                    method: "POST", // *GET, POST, PUT, DELETE, etc.
+                    mode: "cors", // no-cors, cors, *same-origin
+                    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+                    credentials: "same-origin", // include, *same-origin, omit
+                    headers: {
+                        "Content-Type": "application/json; charset=utf-8",
+                        // "Content-Type": "application/x-www-form-urlencoded",
+                    },
+                    redirect: "follow", // manual, *follow, error
+                    referrer: "no-referrer", // no-referrer, *client
+                    body: JSON.stringify(data), // body data type must match "Content-Type" header
+                })
+                .then(response => response.json()); // parses response to JSON
+            }
+
+            return app;
+
+        })(document, window, jQuery, Mustache);
+
+        var spa = new app( document, window, jQuery, Mustache );
+        spa.init();
 	</script>
 
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -18,6 +18,12 @@
 	<!-- jQuery -->
 		<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
 
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/mustache.js/3.0.1/mustache.js"></script>
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
     <!-- Customized JavaScript -->
 
 
@@ -25,64 +31,10 @@
     	<title>@yield('title','f\'lm House')</title>
 	</head>
 	<body>
-    
-        <div class="container-fluid">
-            <nav id="headingNav" class="navbar navbar-expand-md navbar-dark bg-dark sticky-top">
-                <div class="container">
-                    <span class="navbar-text">
-                        <a href="/" class="navbar-brand">f'lm House</a>
-                    </span>
-                    <button class="navbar-toggler" data-toggle="collapse" data-target="#mainNav">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div id="mainNav" class="collapse navbar-collapse ml-auto">
-                        <ul class="navbar-nav ml-auto">
-                            <li class="nav-item">
-                                <a href="/" class="nav-link">Movies</a>
-                            </li>
-{{--                             <li class="nav-item">
-                                <a href="#" class="nav-link">Cinema</a>
-                            </li> --}}
-                            <li class="nav-item">
-                                <a href="/login" class="nav-link">Log-in</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="/register" class="nav-link">Register</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </div>
-    
+
+
         <div class="container">
         	@yield('content')
         </div>
-
-        
-        <div class="container-fluid">
-            
-            <footer id="footer">
-                <div class="container-fluid footer">
-                    <div class="container">
-                        <p>Movies</p>
-                        <ul class="linksList">
-                            <li><a class="link" href="#">Now Playing</a></li>
-                            <li><a class="link" href="#">Coming Soon</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </footer>
-        </div>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <!-- custom JS --> 
-        <script>
-        	
-        </script>   
   	</body>
 </html>
